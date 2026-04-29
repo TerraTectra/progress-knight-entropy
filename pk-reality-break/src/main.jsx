bfa0a0e7efe7b0c3b00a3995d7589ee1da1c8b26
TerraTectra <nik-dom@mail.ru>
2026-04-30 02:03:39 +0300

    style: stop loading legacy theme layers

--- pk-reality-break/src/main.jsx
+++ pk-reality-break/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./AppRuntime.jsx";
-import "./styles.css";
 import "./original-plus.css";
-import "./money-focus.css";
 import { installResourceVisibilityPatch } from "./resourceVisibility.js";
 
 createRoot(document.getElementById("root")).render(<App />);
