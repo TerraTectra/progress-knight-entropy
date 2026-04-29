62bc1f9ff02c0d05ac64be878f9ddd3fbdd8da6a
TerraTectra <nik-dom@mail.ru>
2026-04-29 18:56:15 +0300

    ui: enable money focus layer

--- pk-reality-break/src/main.jsx
+++ pk-reality-break/src/main.jsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./AppRuntime.jsx";
 import "./styles.css";
 import "./medieval.css";
+import "./money-focus.css";
 
 createRoot(document.getElementById("root")).render(<App />);
