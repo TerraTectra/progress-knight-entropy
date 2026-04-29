11fe128b84055551e606d6e258e578ef36355851
TerraTectra <nik-dom@mail.ru>
2026-04-29 19:03:13 +0300

    ui: use original style and gated resource patch

--- pk-reality-break/src/main.jsx
+++ pk-reality-break/src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./AppRuntime.jsx";
 import "./styles.css";
-import "./medieval.css";
+import "./original-plus.css";
 import "./money-focus.css";
+import { installResourceVisibilityPatch } from "./resourceVisibility.js";
 
 createRoot(document.getElementById("root")).render(<App />);
+installResourceVisibilityPatch();
