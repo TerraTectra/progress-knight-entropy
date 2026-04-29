84cdc446086aa113b6e3b0bb37625358648f1a92
TerraTectra <nik-dom@mail.ru>
2026-04-29 18:08:34 +0300

    fix: switch to runtime automation fixes

--- pk-reality-break/src/main.jsx
+++ pk-reality-break/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./AppSmart.jsx";
+import App from "./AppRuntime.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(<App />);
