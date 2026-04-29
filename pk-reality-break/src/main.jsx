ea044ea727cdb0a71f7bb9ea6ad7fc7cf567bff7
TerraTectra <nik-dom@mail.ru>
2026-04-29 17:41:55 +0300

    feat: use smart app runtime

--- pk-reality-break/src/main.jsx
+++ pk-reality-break/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./AppStable.jsx";
+import App from "./AppSmart.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(<App />);
