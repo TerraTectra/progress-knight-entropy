787446c9e2c2b3db02e2461a22dc05d9eec28ad3
TerraTectra <nik-dom@mail.ru>
2026-04-30 02:24:20 +0300

    fix: enable original-style evil rebirth patch

--- pk-reality-break/src/main.jsx
+++ pk-reality-break/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./AppRuntime.jsx";
 import "./original-plus.css";
 import { installResourceVisibilityPatch } from "./resourceVisibility.js";
+import { installEvilRebirthPatch } from "./evilRebirthPatch.js";
 
 createRoot(document.getElementById("root")).render(<App />);
 installResourceVisibilityPatch();
+installEvilRebirthPatch();
